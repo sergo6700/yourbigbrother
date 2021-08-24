cd8ad059295d23cfcd44274d87242321d004023d
Sergo6700 <sergrigoryan98@gmail.com>
2021-08-24 04:45:59 -0400

    hack them all

--- yourbigbrother.sh
+++ yourbigbrother.sh
@@ -15,6 +15,7 @@ command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not instal
 command -v wget > /dev/null 2>&1 || { echo >&2 "I require wget but it's not installed. Install it. Aborting."; exit 1; }
 command -v unzip > /dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Install it. Aborting."; exit 1; }
 command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not installed. Install it. Aborting."; exit 1; }
+command -v ngrok > /dev/null 2>&1 || { echo >&2 "I require ngrok but it's not installed. Install it. Aborting."; exit 1; }
 
 }
 
